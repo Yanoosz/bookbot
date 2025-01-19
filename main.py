584f2cc40938e8eb0ab9ca3113062f75653bfff5
Yanoosz <filip.brandt@gmail.com>
2025-01-19 11:15:46 +0100

    My first project

---
+++ main.py
@@ -0,0 +1,26 @@
+def main():
+    with open("/Users/janoosz/Python_projects/github.com/Yanoosz/bookbot/books/frankenstein.txt") as f:
+        file_contents = f.read()
+
+    words = file_contents.split()
+    words_count = len(words)
+
+    characters_count = {}
+    lower_characters = file_contents.lower()
+    for character in lower_characters:
+        if character not in characters_count:
+            characters_count[character] = 1
+        else:
+            characters_count[character] += 1
+
+    letters = []
+    for key, value in characters_count.items():
+        if key.isalpha():
+            letters.append({"character": key, "count": value})
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words_count} words found in the document")
+    for item in letters:
+        print(f"The '{item['character']}' character was found '{item['count']}' times")
+
+main()
